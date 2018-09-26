3c75ce7f36a4d75774d617dbf4c7a2123073ece8
Savio A <savio.abuga@andela.com>
2018-09-26 10:59:59 +0300

    Inital setup for react

---
+++ src/App.js
@@ -0,0 +1,34 @@
+/* global React ReactDOM */
+
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+class App extends React.Component {
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "bird",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Rex",
+        animal: "dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "bird",
+        breed: "Havanese"
+      })
+    ]);
+  }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
