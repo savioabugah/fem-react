34bbbe54168a3a0fe6a115c10ce9d8b256cbeaa6
Savio A <savio.abuga@andela.com>
2018-09-29 10:33:11 +0300

    feature: add images to Pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,27 @@
 import React from "react";
 
-const Pet = ({ name, animal, breed }) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
-  );
-};
+class Pet extends React.Component {
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+    let photos = [];
 
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} {location}
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
 export default Pet;
