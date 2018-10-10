28a493cba81834a739983ac82188c8c65a616f01
Savio A <savio.abuga@andela.com>
2018-10-10 10:35:40 +0300

    feature: add using portals

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import pf from "petfinder-client";
 import { navigate } from "@reach/router/lib/history";
 import Carousel from "./Carousel";
+import Modal from "./Modal";
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -10,9 +11,12 @@ const petfinder = pf({
 
 class Details extends Component {
   state = {
-    loading: true
+    loading: true,
+    showModal: true
   };
-
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
   componentDidMount() {
     petfinder.pet
       .get({
@@ -46,7 +50,15 @@ class Details extends Component {
       return <h1>Loading... </h1>;
     }
 
-    const { animal, description, breed, location, name, media } = this.state;
+    const {
+      animal,
+      description,
+      breed,
+      location,
+      name,
+      media,
+      showModal
+    } = this.state;
 
     return (
       <div className="details">
@@ -56,7 +68,17 @@ class Details extends Component {
           <h2>
             {animal} {breed} - {location}
           </h2>
+          <button onClick={this.toggleModal}>Adopt {name}</button>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <h1>Would you like to adopt {name}?</h1>
+              <div className="buttons">
+                <button onClick={this.toggleModal}>Yes</button>
+                <button onClick={this.toggleModal}>Definitely Yes</button>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
